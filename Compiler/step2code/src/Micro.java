01b55b6f50e297db24eb00aafe83e02e33513376
Leo Welter <lwelter@purdue.edu>
2016-09-18 16:46:10 -0400

    adds step 2 code for parsing

--- Compiler/step2code/src/Micro.java
+++ Compiler/step2code/src/Micro.java
@@ -6,6 +6,7 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.misc.*;
 import org.antlr.v4.*;
 
 
@@ -16,13 +17,34 @@ public class Micro {
 	  ANTLRFileStream input = new ANTLRFileStream(args[0]);
 		MicroLexer lexer = new MicroLexer(input);
 		//LexerInterpreter lexer = new LexerInterpreter(input);
-		while(true) {
-			Token token = lexer.nextToken();
-			if(token.getType() == Lexer.EOF) {
-				break;
-			}
-			System.out.println("Token Type: " + MicroLexer.tokenNames[token.getType()]);
-			System.out.println("Value: " + token.getText());
+
+		//step2 code
+
+		//lists matched tokens
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+		//tokens -> parser
+		MicroParser parser = new MicroParser(tokens);
+
+		try{
+		parser.program(); //start parser
+
 		}
+		catch(ParseCancellationException err)
+		{
+			System.out.print("Not Accepted");
+		}
+		System.out.print("Accepted");
+
+
+		//step1 code
+		// while(true) {
+		// 	Token token = lexer.nextToken();
+		// 	if(token.getType() == Lexer.EOF) {
+		// 		break;
+		// 	}
+		// 	System.out.println("Token Type: " + MicroLexer.tokenNames[token.getType()]);
+		// 	System.out.println("Value: " + token.getText());
+		// }
 	}
 }
