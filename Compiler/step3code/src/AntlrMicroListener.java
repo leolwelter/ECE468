46cbe3e9a10b3d763d47a2da5dea8c4ec08b6cbc
Leo Bennie Keller Welter <lwelter@ecegrid-thin1.ecn.purdue.edu>
2016-10-07 13:29:19 -0400

    adds proper listener logic

--- Compiler/step3code/src/AntlrMicroListener.java
+++ Compiler/step3code/src/AntlrMicroListener.java
@@ -1,7 +1,11 @@
 public class AntlrMicroListener extends MicroBaseListener {
 
-	@Override public void enterId(MicroParser.IdContext ctx){
-	    System.out.println(ctx.getText());
+	@Override public void enterVar_decl(MicroParser.Var_declContext ctx){
+	    System.out.println(ctx.getText() + "\n");
+	}
+
+	@Override public void enterString_decl(MicroParser.String_declContext ctx) { 
+		System.out.println(ctx.getText() + "\n");
 	}
 
 }
